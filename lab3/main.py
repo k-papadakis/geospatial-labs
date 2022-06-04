3fad9731b78fd7286c66bce06a24c971cdb49b5f
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-06-04 15:34:59 +0300

    Cropping strat. Color info. Transform. Segm displ.

--- lab3/main.py
+++ lab3/main.py
@@ -1,9 +1,12 @@
 # %%
-from typing import Optional, Union, Tuple
-from pathlib import Path
+from typing import Any, Dict, Hashable, Optional, Union, Tuple
+import random
+import csv
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
 import rasterio
 from rasterio.windows import Window
 
@@ -16,7 +19,7 @@ from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import StandardScaler
 
 import torch
-from torch import nn
+from torch import nn, tensor
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader, TensorDataset, ConcatDataset, RandomSampler, Subset, random_split
 import pytorch_lightning as pl
@@ -24,21 +27,23 @@ from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from torchmetrics import Accuracy, ConfusionMatrix
 from torchsummary import summary
 from torchvision.transforms import Compose, RandomHorizontalFlip, RandomVerticalFlip
+import torchvision.transforms.functional as TF 
 
 RANDOM_STATE = 42
 
-# %%
-train_x_paths = [
+INFO_PATH = 'hyrank_info.csv'
+
+TRAIN_X_PATHS = [
     'HyRANK_satellite/TrainingSet/Dioni.tif',
     'HyRANK_satellite/TrainingSet/Loukia.tif',
 ]
 
-train_y_paths = [
+TRAIN_Y_PATHS = [
     'HyRANK_satellite/TrainingSet/Dioni_GT.tif',
     'HyRANK_satellite/TrainingSet/Loukia_GT.tif'
 ]
 
-validation_x_paths = [
+VALINDATION_X_PATHS = [
     'HyRANK_satellite/ValidationSet/Erato.tif',
     'HyRANK_satellite/ValidationSet/Kirki.tif',
     'HyRANK_satellite/ValidationSet/Nefeli.tif',
@@ -64,8 +69,19 @@ def read_data(x_paths, y_paths):
         
     return images, labels
 
+def read_info(path) -> Dict[int, Dict]:
+    with open(INFO_PATH) as f:
+        reader = csv.DictReader(f)
+        names = reader.fieldnames[1:]
+        info = {field: [] for field in names}
+        for record in reader:
+            for name in names:
+                info[name].append(record[name])
+    return info
 
-images, label_images = read_data(train_x_paths, train_y_paths)
+images, label_images = read_data(TRAIN_X_PATHS, TRAIN_Y_PATHS)
+info = read_info(INFO_PATH)
+cmap = ListedColormap(info['color'])
 
 # %%
 # STEP 1 - PLOT SAMPLES
@@ -234,16 +250,21 @@ class PatchDataset(Dataset):
 class AugmentedDataset(Dataset):
     """"Wraps a dataset to include a transform"""
     
-    def __init__(self, dataset, transform):
+    def __init__(self, dataset, transform, apply_on_target=False):
         self.dataset = dataset
         self.transform = transform
+        self.apply_on_target = apply_on_target
         
     def __len__(self):
         return len(self.dataset)
     
     def __getitem__(self, idx):
         x, y = self.dataset[idx]
-        return self.transform(x), y
+        if self.apply_on_target:
+            x, y = self.transform(x, y)
+        else:
+            x = self.transform(x)
+        return x, y
 
 
 def display_patch(dataset, idx, ax=None, rgb=(23, 11, 7)):
@@ -257,7 +278,7 @@ def display_patch(dataset, idx, ax=None, rgb=(23, 11, 7)):
     ax.set_axis_off()
     return ax
 
-
+# %%
 def split_dataset(dataset, train_size, test_size=0., seed=None):
     if not 0 <= train_size + test_size <= 1:
         raise ValueError('Invalid train/test sizes')
@@ -273,9 +294,32 @@ def split_dataset(dataset, train_size, test_size=0., seed=None):
     return dataset_train, dataset_val, dataset_test
 
 
-patch_size = 15
-transform = Compose([RandomHorizontalFlip(), RandomVerticalFlip()])
+def flip_and_rotate(*tensors):
+    # Use this transform only when height == width!
+    
+    tensors = list(tensors)
+    # Expand 2 dimensional tensors so that the transforms work
+    expanded = []
+    for i in range(len(tensors)):
+        if tensors[i].ndim == 2:
+            tensors[i] = torch.unsqueeze(tensors[i], 0)
+            expanded.append(i)
+    
+    # Flip vertically
+    if random.random() > 0.5:
+        tensors = [TF.vflip(t) for t in tensors]
+    # Rotate by 0, 1, 2, or 3 right angles 
+    if (k := random.randint(0, 4)) != 0:
+        tensors = [TF.rotate(t, k * 90) for t in tensors]
+    
+    # Undo the expansion
+    for idx in expanded:
+        tensors[idx] = torch.squeeze(tensors[i], 0)
 
+    return tuple(tensors) if len(tensors) > 1 else tensors[0]
+
+
+patch_size = 15
 patch_dataset = ConcatDataset([
     PatchDataset(image, label_image, patch_size)
     for image, label_image in zip(images, label_images)
@@ -284,18 +328,18 @@ patch_dataset = ConcatDataset([
 patch_dataset_train, patch_dataset_val, patch_dataset_test = split_dataset(
     patch_dataset, train_size=0.8, test_size=0.1, seed=RANDOM_STATE
 )
-patch_dataset_train_aug = AugmentedDataset(patch_dataset_train, transform)
+patch_dataset_train_aug = AugmentedDataset(patch_dataset_train, flip_and_rotate)
 
 # fig, axs = plt.subplots(5, 5, figsize=(12, 12))
 # for ax in axs.flat:
-#     display_patch(dataset_train_aug, 200, ax=ax)
+#     display_patch(patch_dataset_train_aug, 200, ax=ax)
 #     ax.set_axis_off()
 
 
-loader_train = DataLoader(patch_dataset_train, batch_size=64, shuffle=True, num_workers=8)
-loader_train_aug = DataLoader(patch_dataset_train_aug, batch_size=64, shuffle=True, num_workers=8)
-loader_val = DataLoader(patch_dataset_val, batch_size=64, num_workers=8)
-loader_test = DataLoader(patch_dataset_test, batch_size=64, num_workers=8)
+patch_loader_train = DataLoader(patch_dataset_train, batch_size=64, shuffle=True, num_workers=8)
+patch_loader_train_aug = DataLoader(patch_dataset_train_aug, batch_size=64, shuffle=True, num_workers=8)
+patch_loader_val = DataLoader(patch_dataset_val, batch_size=64, num_workers=8)
+patch_loader_test = DataLoader(patch_dataset_test, batch_size=64, num_workers=8)
 
 
 # %% 
@@ -405,10 +449,10 @@ def train_cnn():
         callbacks=callbacks,
         default_root_dir='cnn_results'
     )
-    trainer.fit(model, train_dataloaders=loader_train_aug, val_dataloaders=loader_val)
+    trainer.fit(model, train_dataloaders=patch_loader_train_aug, val_dataloaders=patch_loader_val)
 
-    y_true = torch.cat([y for x, y in loader_test], 0)
-    y_pred = torch.cat(trainer.predict(dataloaders=loader_test), 0)
+    y_true = torch.cat([y for x, y in patch_loader_test], 0)
+    y_pred = torch.cat(trainer.predict(dataloaders=patch_loader_test), 0)
     print(classification_report(y_true, y_pred))
     
     return trainer
@@ -422,7 +466,7 @@ rgb_dataset = ConcatDataset([
 rgb_dataset_train, rgb_dataset_val, rgb_dataset_test = split_dataset(
     rgb_dataset, train_size=0.8, test_size=0.1, seed=RANDOM_STATE
 )
-rgb_dataset_train_aug = AugmentedDataset(rgb_dataset_train, transform)
+rgb_dataset_train_aug = AugmentedDataset(rgb_dataset_train, flip_and_rotate)
 
 rgb_loader_train = DataLoader(rgb_dataset_train, batch_size=64, shuffle=True, num_workers=8)
 rgb_loader_train_aug = DataLoader(rgb_dataset_train_aug, batch_size=64, shuffle=True, num_workers=8)
@@ -454,26 +498,32 @@ class CroppedDataset(Dataset):
         if self.crop_h > self.img_h or self.crop_w > self.img_w:
             raise ValueError('crops_size is bigger than image size.')
         
-        self.channels = channels if channels is not None else slice(None)
+        self.channels = channels
         self.transform = transform
         
     def __len__(self):
-        return (self.img_h - self.crop_h + 1) * (self.img_w - self.crop_w + 1)
+        return self.n_rows * self.n_cols
     
     def __getitem__(self, idx):
-        min_i, min_j = divmod(idx, self.img_w - self.crop_w + 1)
-        max_i, max_j = min_i + self.crop_h, min_j + self.crop_w  # non inclusive
-        
-        x = self.image[self.channels, min_i : max_i, min_j : max_j]
+        min_i, min_j, max_i, max_j = self.index_to_bounds(idx)
+        channels = self.channels if self.channels is not None else slice(None)
+        x = self.image[channels, min_i : max_i, min_j : max_j]
         y = self.labels[min_i : max_i, min_j : max_j]
         
         x = torch.tensor(x, dtype=torch.float32)
         y = torch.tensor(y, dtype=torch.int64)
         
-        if self.transform is not None:
-            x = self.transform(x)
+        if self.transform:
+            x, y = self.transform(x, y)
             
         return x, y
+    
+    def index_to_bounds(self, idx):
+        min_i, min_j = divmod(idx, self.n_cols)
+        # Max bound is exclusive
+        max_i = min_i + self.crop_h
+        max_j = min_j + self.crop_w
+        return min_i, min_j, max_i, max_j
         
     @property
     def img_h(self):
@@ -482,26 +532,68 @@ class CroppedDataset(Dataset):
     @property
     def img_w(self):
         return self.image.shape[-1]
+    
+    @property
+    def n_rows(self):
+        return self.img_h - self.crop_h + 1
+    
+    @property
+    def n_cols(self):
+        return self.img_w - self.crop_w + 1
 
 
-# %% 
+# %%
+# One image has height 249 while the other has 250
+# We will use crop_height 249 for both,
+# and ignore the last row of pixels of
+# the image with height 250.
+# The crop width will be equal to crop_height = 249.
+# We will slide the crop window on the first row with
+# step crop_width // 4 so that there is some overlap.
 crop_size = 249
-cropped_dataset = ConcatDataset([
-    CroppedDataset(image, labels_image, crop_size=crop_size)
-    for image, labels_image in zip(images, label_images)
-])
-indices = range(0, len(cropped_dataset), len(cropped_dataset) // 99)  # 102 points
-cropped_dataset = Subset(cropped_dataset, indices)
-# display_patch(cropped_dataset, 10)
+cropped_datasets_lst = []
+for image, label_image in zip(images, label_images):
+    ds = CroppedDataset(image, label_image, crop_size)
+    indices = range(0, ds.n_cols, ds.crop_w // 4)
+    ds = Subset(ds, indices)
+    cropped_datasets_lst.append(ds)
+cropped_dataset = ConcatDataset(cropped_datasets_lst)
+# display_patch(cropped_dataset, 0)
+# display_patch(cropped_dataset, 1)
 
 cropped_dataset_train, cropped_dataset_val, cropped_dataset_test = split_dataset(
-    cropped_dataset, train_size=0.8, test_size=0.1, seed=RANDOM_STATE
+    cropped_dataset, train_size=0.7, test_size=0.2, seed=RANDOM_STATE
 )
-cropped_dataset_train_aug = AugmentedDataset(cropped_dataset_train, transform)
+    
+cropped_dataset_train_aug = AugmentedDataset(
+    cropped_dataset_train, transform=flip_and_rotate, apply_on_target=True
+)
+# print(*map(len, (cropped_dataset_train_aug, cropped_dataset_val, cropped_dataset_test)))
 
-cropped_loader_train = DataLoader(cropped_dataset_train, batch_size=1, shuffle=True, num_workers=0)
-cropped_loader_train_aug = DataLoader(cropped_dataset_train_aug, batch_size=1, shuffle=True, num_workers=0)
-cropped_loader_val = DataLoader(cropped_dataset_val, batch_size=1, num_workers=0)
-cropped_loader_test = DataLoader(cropped_dataset_test, batch_size=1, num_workers=0)
+cropped_loader_train_aug = DataLoader(cropped_dataset_train_aug, batch_size=1, shuffle=True, num_workers=1)
+cropped_loader_val = DataLoader(cropped_dataset_val, batch_size=1, num_workers=1)
+cropped_loader_test = DataLoader(cropped_dataset_test, batch_size=1, num_workers=1)
+    
+# %%
+def display_segmentation(dataset, idx, rgb=(23, 11, 7), cmap=None, axs=None):
+    if axs is None:
+        fig, axs = plt.subplots(ncols=2)
+        
+    img, label = dataset[idx]
+    img = img[rgb, ...]
+    img = img / img.max()
+    img = torch.permute(img, (1, 2, 0))
+    
+    axs[0].imshow(img)
+    axs[1].imshow(label, cmap=cmap)
+    
+    axs[0].set_axis_off()
+    axs[1].set_axis_off()
+    
+    return axs
+
+# fig, axs = plt.subplots(4, 2, figsize=(4, 8))
+# for ax1, ax2 in axs:
+#     display_segmentation(cropped_dataset_train_aug, 0, cmap=cmap, axs=(ax1, ax2))
 
 # %%
